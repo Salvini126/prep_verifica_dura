94e760ab615daf062e7115039ed1e12504e238cd
Salvini126 <salvini.matteo@iisgalvanimi.edu.it>
2021-12-09 15:24:27 +0000

    tutti i punti

--- prep-ver-dura/src/app/container/container.component.ts
+++ prep-ver-dura/src/app/container/container.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-container',
   templateUrl: './container.component.html',
   styleUrls: ['./container.component.css']
 })
-export class UserListComponent implements OnInit {
-  names: string[]; 
+export class ContainerComponent implements OnInit {
+  names: string[];
   constructor() { 
-     this.names = ['Escavatori', 'Bulldozer.', 'Motolivellatrici', 'Abbattitrici forestali','Macchine di demolizione','Camion betoniere','Dumper','Macchine per miniere sotterranee','Esboscatrici','Gru forestali',]; 
+    this.names = this.names = ['Escavatori', 'Bulldozer.', 'Motolivellatrici', 'Abbattitrici forestali','Macchine di demolizione','Camion betoniere','Dumper','Macchine per miniere sotterranee','Esboscatrici','Gru forestali',];  //Riempiamo il vettore
   }
-  ngOnInit() {
+
+  ngOnInit(): void {
   }
+
 }
